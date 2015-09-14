af0e734e6fdac8bef625312cce4a2003bb2b60ae
William BONNET <william@wbonnet.net>
2015-09-14 22:54:37 +0200

    Project initialization

---
+++ sources/core/src/main/java/com/theitmakers/lutefisk/core/App.java
@@ -0,0 +1,13 @@
+package com.theitmakers.lutefisk.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
